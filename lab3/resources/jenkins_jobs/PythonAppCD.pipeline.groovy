d0b07a3c197f38031490018121bad9cd62dfedaf
painterhalver <hieptrang13042002@gmail.com>
2024-04-29 11:18:34 +0700

    lab3: update PythonAppCD to use only app module

--- lab3/resources/jenkins_jobs/PythonAppCD.pipeline.groovy
+++ lab3/resources/jenkins_jobs/PythonAppCD.pipeline.groovy
@@ -6,29 +6,34 @@ pipeline {
         stage('Checkout') {
             steps {
                 git branch: 'master', url: 'https://github.com/PainterHalver/sc_lab.git'
+            }
+        }
+        // Get the latest git commit hash of repo sc_lab3_app, not this sc_lab repo
+        stage('Get latest git commit hash') {
+            steps {
                 script {
-                    env.GIT_COMMIT = sh(script: 'git rev-parse HEAD', returnStdout: true).trim()
+                    env.GIT_COMMIT = sh(script: 'git ls-remote https://github.com/painterhalver/sc_lab3_app.git HEAD | awk \'{ print $1 }\'', returnStdout: true).trim()
                 }
             }
         }
         stage('Init and validate') {
             steps {
-                dir('lab3/app') {
-                    sh 'terraform init -reconfigure'
+                dir('lab3') {
+                    sh 'terraform init -migrate-state'
                     sh "terraform validate"
                 }
             }
         }
         stage('Plan') {
             steps {
-                dir('lab3/app') {
-                    sh "terraform plan -var app_git_commit_hash=${GIT_COMMIT} -out=tfplan"
+                dir('lab3') {
+                    sh "terraform plan -target=module.app -var app_git_commit_hash=${GIT_COMMIT} -out=tfplan"
                 }
             }
         }
         stage('Apply') {
             steps {
-                dir('lab3/app') {
+                dir('lab3') {
                     sh "terraform apply -auto-approve tfplan"
                 }
             }
